fc7b9a62cb9f45e9f1695a9c3b44d4caf321c96f
sahith <bandasahithreddy@gmail.com>
2021-05-19 06:59:01 +0530

    database added

--- server.js
+++ server.js
@@ -26,27 +26,27 @@ app.get('/',function(req,res)
 })
 
 
-var tasks=[];
-var idx=0;
+// var tasks=[];
+// var idx=0;
 
-app.post('/post',function(req,res)
-{
+// app.post('/post',function(req,res)
+// {
     
-    var x=req.body;
-    tasks.push(x);
-    x['id']=idx;
-    res.send(x);
-    idx++;
-})
+//     var x=req.body;
+//     tasks.push(x);
+//     x['id']=idx;
+//     res.send(x);
+//     idx++;
+// })
 
-app.delete('/del/:id',function(req,res)
-{
-    console.log(req.params.id)
+// app.delete('/del/:id',function(req,res)
+// {
+//     console.log(req.params.id)
 
-    tasks=tasks.filter((x)=>{return (x.id!=req.params.id)});
-    res.send("deleted");
+//     tasks=tasks.filter((x)=>{return (x.id!=req.params.id)});
+//     res.send("deleted");
 
-})
+// })
 
 
 
